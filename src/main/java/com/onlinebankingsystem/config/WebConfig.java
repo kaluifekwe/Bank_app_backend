ca58bf7e15e0bf27b257855718e25a5c4e33c454
smart <smart@digitalwitchng.online>
2024-12-28 12:13:29 +0100

    updated backend

--- src/main/java/com/onlinebankingsystem/config/WebConfig.java
+++ src/main/java/com/onlinebankingsystem/config/WebConfig.java
@@ -16,30 +16,30 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class WebConfig {
 
-    // private static final Long MAX_AGE = 3600L;
-    // private static final int CORS_FILTER_ORDER = -102;
+    private static final Long MAX_AGE = 3600L;
+    private static final int CORS_FILTER_ORDER = -102;
 
-    // @Bean
-    // public FilterRegistrationBean<CorsFilter> corsFilter() {
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     CorsConfiguration config = new CorsConfiguration();
-    //     config.setAllowCredentials(true);
-    //     config.addAllowedOrigin("*");
-    //     config.setAllowedHeaders(Arrays.asList(
-    //             HttpHeaders.AUTHORIZATION,
-    //             HttpHeaders.CONTENT_TYPE,
-    //             HttpHeaders.ACCEPT));
-    //     config.setAllowedMethods(Arrays.asList(
-    //             HttpMethod.GET.name(),
-    //             HttpMethod.POST.name(),
-    //             HttpMethod.PUT.name(),
-    //             HttpMethod.DELETE.name()));
-    //     config.setMaxAge(MAX_AGE);
-    //     source.registerCorsConfiguration("/**", config);
-    //     FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://178.128.37.221:3000");
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        config.setMaxAge(MAX_AGE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
 
-    //     // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
-    //     bean.setOrder(CORS_FILTER_ORDER);
-    //     return bean;
-    // }
+        // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
+        bean.setOrder(CORS_FILTER_ORDER);
+        return bean;
+    }
 }
